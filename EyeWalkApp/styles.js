be6e7d6e6d3a64d3570345abdeb4f944eafe5510
Hana Lim <62918674+pioneer0317@users.noreply.github.com>
2021-05-30 09:08:36 -0700

    Edited profile pages and text fonts (#10)
    * Edited profile pages and text fonts
    
    * Fixed Profile Shenanigans
    
    Co-authored-by: Matthew Nieva <matthewcn56@gmail.com>

--- EyeWalkApp/styles.js
+++ EyeWalkApp/styles.js
@@ -82,6 +82,26 @@ export default StyleSheet.create({
     margin: 8,
   },
 
+  Button: {
+    alignItems: "center",
+    width: "68%",
+    backgroundColor: themedBlue,
+    padding: 20,
+    borderRadius: 10,
+    margin: 35,
+    top: "-7%",
+  },
+
+  profileButton: {
+    alignItems: "center",
+    width: "-19%",
+    backgroundColor: themedBlue,
+    padding: 20,
+    borderRadius: 10,
+    margin: 10,
+    top: "10%",
+  },
+
   rectangle: {
     height: "32%",
     display: "flex",
@@ -93,6 +113,23 @@ export default StyleSheet.create({
     top: 0,
     position: "absolute",
     width: "100%",
+  },
+
+  lowerProfileRectangle: {
+    marginTop: windowHeight / 2.5,
+  },
+
+  profileScroll: {
+    alignItems: "center",
+    flexDirection: "column",
+    flexGrow: 1,
+    width: windowWidth,
+    marginTop: windowHeight / 4,
+  },
+  containerscroll: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    flexDirection: "column",
   },
 
   routingButton: {
@@ -151,15 +188,27 @@ export default StyleSheet.create({
   },
 
   input: {
-    width: "50%",
-    borderWidth: 1,
+    width: "70%",
+    borderWidth: 0.5,
     borderColor: "black",
     textAlign: "center",
+    opacity: 50,
+    padding: 7,
+    borderRadius: 10,
+    margin: 8,
+    top: "-8%",
   },
 
   inputPrompt: {
-    width: "30%",
+    width: "50%",
+    textAlign: "left",
+    alignSelf: "flex-start",
+    fontSize: 16,
+    top: "-15%",
+    fontSize: 16,
+    fontFamily: "Quicksand_500Medium",
   },
+
   map: {
     display: "flex",
     //height: (windowHeight * 65) / 100,
